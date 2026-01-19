dc6aae7adeb5d6b892a184dadf674eed6ab4e969
phatus <trezeq@gmail.com>
2026-01-19 15:15:52 +0700

    Redirect to homepage (/) after logout

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -165,7 +165,7 @@ export default function Header({ sidebarOpen, setSidebarOpen, user }: HeaderProp
                                 <button
                                     onClick={async () => {
                                         await signOut({ redirect: false })
-                                        window.location.href = "/login"
+                                        window.location.href = "/"
                                     }}
                                     className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
                                 >
